17db16343d5d3132a157f146bc1e3adae9c49dc9
Yibay <514531536@qq.com>
2016-05-29 23:10:12 +0800

    module route change

--- js/route.js
+++ js/route.js
@@ -1,6 +1,6 @@
 app.config(["$routeProvider",function ($routeProvider) {
 	$routeProvider.when("/",{templateUrl: "template/home.html"})
-	.when("/module",{templateUrl: "template/list.html"})
+	.when("/module",{template: "module"})
 	.when("/web",{template: "web"})
 	.when("/course",{template: "course"})
 	.otherwise({redirectTo: "/"});
