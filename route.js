98ba5656c9e6c4c8cf11d1dad430437917d51cf2
Yibay <514531536@qq.com>
2016-05-29 17:00:19 +0800

    route list change

--- route.js
+++ route.js
@@ -1,6 +1,6 @@
 app.config(["$routeProvider",function ($routeProvider) {
 	$routeProvider.when("/",{template: 1111})
-	.when("/list",{templateUrl: "https://github.com/Yibay/netease-education/tree/gh-pages/css/index.html"})
+	.when("/list",{templateUrl: "route.js"})
 	.when("/form",{template: "form"})
 	.otherwise({redirectTo: "/"});
 }]);
